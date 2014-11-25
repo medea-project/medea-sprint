cbee9107727fca988a5531e7445138f6b01ac778
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2014-11-25 14:24:26 +0100

    Rename old Doughnut project

---
+++ prototype/authors-by-wg/sandbox.js
@@ -0,0 +1,13 @@
+;(function(ns, $, undefined){
+  'use strict';
+  
+  ns.sourceFile = 'france_authors_by_ar_wg.csv'
+  
+  var width = 960
+  ,height = 500
+  ,radius = Math.min(width, height) / 2
+
+  
+
+
+})(window.script = window.script || {}, jQuery)
--- prototype/doughnut-viz/sandbox.js
+++
@@ -1,7 +0,0 @@
-;(function(ns, $, undefined){
-  'use strict';
-  
-  ns.sourceFile = 'XXX.csv'
-  
-
-})(window.script = window.script || {}, jQuery)
