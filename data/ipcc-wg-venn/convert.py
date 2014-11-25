1c97eceba2eba405d12367ca9b4d2c8b3288ea42
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-11-25 16:32:12 +0100

    adapt json format to data expected by venn lib

--- data/ipcc-wg-venn/convert.py
+++ data/ipcc-wg-venn/convert.py
@@ -1,33 +1,45 @@
 #!/usr/bin/env python
 
-import json
+import json, copy
 
 data = {}
 emptyset = {
-  "WG 1": 0, "WG 2": 0, "WG 3": 0,
-  "WG 1&2": 0, "WG 1&3": 0, "WG 2&3": 0,
-  "WG 1&2&3": 0
+  "sets": [
+    {"label": "WG 1", "size": 0},
+    {"label": "WG 2", "size": 0},
+    {"label": "WG 3", "size": 0}
+  ],
+  "overlaps": [
+    {"label": "WG 1&2", "sets": [0, 1], "size": 0},
+    {"label": "WG 1&3", "sets": [0, 2], "size": 0},
+    {"label": "WG 2&3", "sets": [1, 2], "size": 0},
+    {"label": "WG 1&2&3", "sets": [0, 1, 2], "size": 0}
+  ]
 }
 
 def filler(dico, gr, va):
     va = int(va)
     if "1+2+3" in gr:
-        for k in dico:
-            dico[k] += va
+        for k in dico["sets"]:
+            k["size"] += va
+        for k in dico["overlaps"]:
+            k["size"] += va
     elif "+" in gr:
-        figs = sorted(gr.lstrip("WG").split("+"))
-        dico["WG %s&%s" % (figs[0], figs[1])] += va
+        figs = sorted([int(i) for i in gr.lstrip("WG").split("+")])
         for i in figs:
-            dico["WG %s" % i] += va
+            dico["sets"][i-1]["size"] += va
+        for k in dico["overlaps"]:
+            if k["label"] == "WG %s&%s" % (figs[0], figs[1]):
+                k["size"] += va
     else:
-        dico["WG %s" % gr.lstrip("WG")] += va
+        dico["sets"][int(gr.lstrip("WG"))-1]["size"] += va
 
 with open("participations_by_ar_wg_cumulated_in_same_ar.csv") as f:
     for line in f.read().split("\n")[1:-1]:
         ar, gr, va = line.split(",")
         ar = "AR-%s" % ar
         if ar not in data:
-            data[ar] = dict(emptyset)
+            data[ar] = copy.deepcopy(emptyset)
         filler(data[ar], gr, va)
 
 for fil, ar in [
@@ -36,7 +48,7 @@ for fil, ar in [
   ("distinct", "AR-inter"),
 ]:
     with open("participations_by_wg_cumulated_in_%s_ar.csv" % fil) as f:
-        data[ar] = dict(emptyset)
+        data[ar] = copy.deepcopy(emptyset)
         for line in f.read().split("\n")[1:-1]:
             gr, va = line.split(",")
             filler(data[ar], gr, va)
