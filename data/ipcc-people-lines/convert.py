fc16a1f8e7451e053468f20041295aab01bc7f5c
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-11-27 16:04:40 +0100

    add first ar meta in data

--- data/ipcc-people-lines/convert.py
+++ data/ipcc-people-lines/convert.py
@@ -35,6 +35,7 @@ with open("participations.csv") as f:
               "institution": clean(l["Institution (INFO)"]),
               "department": clean(l["Department (INFO)"]),
               "total_ars": 0,
+              "first_ar": 0,
               "ar1": {
                 "total": 0,
                 "participations": []
@@ -57,6 +58,8 @@ with open("participations.csv") as f:
               }
             }
         arid = "ar%s" % l["ar"]
+        if not countries[cnt][l["author_id"]]["first_ar"]:
+            countries[cnt][l["author_id"]]["first_ar"] = int(l["ar"])
         if not countries[cnt][l["author_id"]][arid]["total"]:
             countries[cnt][l["author_id"]]["total_ars"] += 1
         countries[cnt][l["author_id"]][arid]["total"] += 1
